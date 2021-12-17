69d94b3fd407d3a10e9e07357f99d336e6274d48
indraranuh-dev <indraranuh1@gmail.com>
2021-12-17 15:24:05 +0700

    Update new method and correct activeRouteIs mehtod

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
-function activeRouteIs($routeName, $active = 'active')
+function activeRouteIs($routeName, $active = 'mm-active')
 {
     return request()->routeIs($routeName) ? $active : '';
 }
@@ -63,4 +63,31 @@ function user($column = null)
     }
 
     return $user ? $user : null;
+}
+
+function numberShortner($n)
+{
+    if ($n >= 0 && $n < 1000) {
+        // 1 - 999
+        $n_format = floor($n);
+        $suffix = '';
+    } else if ($n >= 1000 && $n < 1000000) {
+        // 1k-999k
+        $n_format = floor($n / 1000);
+        $suffix = 'K+';
+    } else if ($n >= 1000000 && $n < 1000000000) {
+        // 1m-999m
+        $n_format = floor($n / 1000000);
+        $suffix = 'M+';
+    } else if ($n >= 1000000000 && $n < 1000000000000) {
+        // 1b-999b
+        $n_format = floor($n / 1000000000);
+        $suffix = 'B+';
+    } else if ($n >= 1000000000000) {
+        // 1t+
+        $n_format = floor($n / 1000000000000);
+        $suffix = 'T+';
+    }
+
+    return !empty($n_format . $suffix) ? $n_format . $suffix : 0;
 }
